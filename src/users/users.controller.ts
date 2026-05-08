f77dafe434a9d1e8db094920f30493a8b2189398
ahmewagih <ahmedwagih087@gmail.com>
2026-05-08 23:51:08 +0300

    create the user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get() // GET /users
+  findAll(@Query('role') role?: 'ADMIN' | 'USER') {
+    return `This action returns all users ${role ? `with role ${role}` : ''}`;
+  }
+
+  @Post() // POST /users
+  create(@Body() user: { name: string }) {
+    return user;
+  }
+
+  @Get(':id') // GET /users/:id
+  findOne(@Param('id') id: string) {
+    return `This action returns a user with id: ${id}`;
+  }
+
+  @Patch(':id') // PATCH /users/:id
+  update(@Param('id') id: string, @Body() user: { name: string }) {
+    return user;
+  }
+
+  @Delete(':id') // DELETE /users/:id
+  remove(@Param('id') id: string) {
+    return `This action removes a user with id: ${id}`;
+  }
+}
